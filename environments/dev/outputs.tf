ade9bd7af20294bf565bb56b11f61c65ddadd38d
Kaushik Manivannan <manivannan.k@northeastern.edu>
2024-10-09 22:16:33 -0400

    Feat: Create Outputs in Dev Environment

---
+++ environments/dev/outputs.tf
@@ -0,0 +1,44 @@
+output "vpc_ids" {
+  description = "Map of VPC IDs"
+  value       = { for k, v in module.networking : k => v.vpc_id }
+}
+
+output "vpc_cidr_blocks" {
+  description = "Map of VPC CIDR blocks"
+  value       = { for k, v in module.networking : k => v.vpc_cidr_block }
+}
+
+output "public_subnet_ids" {
+  description = "Map of public subnet IDs for each VPC"
+  value       = { for k, v in module.networking : k => v.public_subnet_ids }
+}
+
+output "public_subnet_cidr_blocks" {
+  description = "Map of public subnet CIDR blocks for each VPC"
+  value       = { for k, v in module.networking : k => v.public_subnet_cidr_blocks }
+}
+
+output "private_subnet_ids" {
+  description = "Map of private subnet IDs for each VPC"
+  value       = { for k, v in module.networking : k => v.private_subnet_ids }
+}
+
+output "private_subnet_cidr_blocks" {
+  description = "Map of private subnet CIDR blocks for each VPC"
+  value       = { for k, v in module.networking : k => v.private_subnet_cidr_blocks }
+}
+
+output "internet_gateway_ids" {
+  description = "Map of Internet Gateway IDs for each VPC"
+  value       = { for k, v in module.networking : k => v.internet_gateway_id }
+}
+
+output "public_route_table_ids" {
+  description = "Map of public route table IDs for each VPC"
+  value       = { for k, v in module.networking : k => v.public_route_table_id }
+}
+
+output "private_route_table_ids" {
+  description = "Map of private route table IDs for each VPC"
+  value       = { for k, v in module.networking : k => v.private_route_table_id }
+}
