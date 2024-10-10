6bef54b942106a3f7ab077bea9a7153ecc89184c
Kaushik Manivannan <manivannan.k@northeastern.edu>
2024-10-10 04:13:43 -0400

    Test: Add Invalid Resource for Testing Validation

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -84,3 +84,7 @@ resource "aws_route_table_association" "private" {
   subnet_id      = aws_subnet.private[count.index].id
   route_table_id = aws_route_table.private.id
 }
+
+resource "invalid_resource" "test" {
+  name = "test"
+}
