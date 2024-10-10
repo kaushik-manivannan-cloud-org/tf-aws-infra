15074fc5106731d1fac0b74a25f5467e3f6ec6b7
Kaushik Manivannan <manivannan.k@northeastern.edu>
2024-10-09 22:15:30 -0400

    Feat: Associate Route Table with Subnets

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -71,3 +71,16 @@ resource "aws_route_table" "private" {
     Environment = var.environment
   }
 }
+
+# Associate route tables with subnets
+resource "aws_route_table_association" "public" {
+  count          = length(aws_subnet.public)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "private" {
+  count          = length(aws_subnet.private)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
+}
