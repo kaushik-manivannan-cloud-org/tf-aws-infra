f96d003b5b12bb8988e5b34fb629d822c1be2583
Kaushik Manivannan <manivannan.k@northeastern.edu>
2024-11-30 00:03:41 -0500

    Feat: Modify EC2 Policy for KMS and Secrets

--- modules/iam/variables.tf
+++ modules/iam/variables.tf
@@ -12,3 +12,28 @@ variable "sns_topic_arn" {
   description = "ARN of the SNS topic"
   type        = string
 }
+
+variable "ec2_kms_key_arn" {
+  description = "ARN of the EC2 KMS key"
+  type        = string
+}
+
+variable "rds_kms_key_arn" {
+  description = "ARN of the RDS KMS key"
+  type        = string
+}
+
+variable "s3_kms_key_arn" {
+  description = "ARN of the S3 KMS key"
+  type        = string
+}
+
+variable "secrets_kms_key_arn" {
+  description = "ARN of the Secrets KMS key"
+  type        = string
+}
+
+variable "db_credentials_arn" {
+  description = "ARN of the database credentials secret"
+  type        = string
+}
